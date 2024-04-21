eff4243ad67a10acf823b09d4015c19f149f1c23
Kingakr <155776214+Kingakr@users.noreply.github.com>
2024-04-21 13:04:00 +0200

    summary

--- task-2.js
+++ task-2.js
@@ -1,11 +1,9 @@
 function getShippingMessage(country, price, deliveryFee) {
-  return (totalPrice = price + deliveryFee);
-  console.log(`Shipping to ${country} will cost ${totalPrice} credits!`);
+  console.log(
+    `Shipping to ${country} will cost ${price + deliveryFee} credits!`
+  );
 }
 
 getShippingMessage("Australia", 120, 50);
 getShippingMessage("Germany", 80, 20);
 getShippingMessage("Sweden", 100, 20);
-
-// Mimo oglądania lekcji i czytania konspektu nadal nie wiem jak to zrobić i potrzebuję żeby ktoś mi to logicznie wytłumaczył.
-// Nie wiem jak zapisać obliczenie dla totalPrice, siedziałam nad tym kilka dni i nie wiem.
--- task-3.js
+++ task-3.js
@@ -1,18 +1,12 @@
 function getElementWidth(content, padding, border) {
-  const a = content.replace("px", ""),
-    b = padding.split("px")[0],
-    c = border.slice(0, border.lenght - 2);
+  const contentWidth = parseFloat(content);
+  const paddingWidth = parseFloat(padding);
+  const borderWidth = parseFloat(border);
 
-  console.log(a, b, c);
-  const elementWidth = Number(a) + Number(b) + Number(c);
-  console.log(elementWidth);
+  const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
+  return Math.round(totalWidth);
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
-
-// Zrobiłam wg przykładu z drugich zajęć z Kubą ale nie wiem dlaczego nie wyświetla bordera.
-// Kuba na tych zajęciach nie tłumaczył co to replace, split, slice i za co odpowiadają "0" w nawiasach
-// więc zrobiłam ale nie do końca wiem o co chodzi.
-// Mimo oglądania lekcji i czytania konspektu nadal nie do końca to rozumiem i potrzebuję żeby ktoś mi to logicznie wytłumaczył.
